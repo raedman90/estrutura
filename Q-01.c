59882798efa006a41c5b5451c0ecfed7a0ffa013
Israel Oliveira <35226183+raedman90@users.noreply.github.com>
2021-04-18 21:55:47 -0300

    Add files via upload

--- Q-01.c
+++ Q-01.c
@@ -15,7 +15,6 @@ int linhaB = 2, colunaB = 3;
 
 int tamanhoMatriz1 = 6;
 int tamanhoMatriz2 = 6;
-
 //Adicionando valores a matriz [A]
 void AdicionarMatriz1( int mat1[tamanhoMatriz1] ) {
         printf("\nPreenchendo a matriz [A] %d - %d\n\n",linhaA,colunaA);
@@ -42,6 +41,7 @@ void AdicionarMatriz2( int mat2[tamanhoMatriz2] ) {
             printf("\n");
         }
 }
+
 //Calculando produto das matrizes [A] E [B]
 void produtoMatrizes( int mat1[tamanhoMatriz1], int mat2[tamanhoMatriz2] ) {
         int lA = 0, lB = 0;
@@ -49,46 +49,114 @@ void produtoMatrizes( int mat1[tamanhoMatriz1], int mat2[tamanhoMatriz2] ) {
         int posA = 0, posB = 0;
         int soma = 0;        
         
-        for( cA = 0; cA < tamanhoMatriz1/2; cA++ ) { 
+        //Primeira linha
+        for( cA = 0; cA < tamanhoMatriz1/3; cA++ ) { 
             posA = lA*colunaA + cA; 
             posB = lB*colunaB + cB; 
-            soma = soma + (mat1[posA])*(mat2[posB]); 
+            soma = soma + (mat1[posA])*(mat2[posB]);
             lB = lB + 1; 
         }
-
         printf("%d ", soma);
+        
         lA = 0;
         lB = 0; 
         cB = 1;
         soma = 0;
 
-        for( cA = 0; cA < tamanhoMatriz1/2; cA++ ) {
+        for( cA = 0; cA < tamanhoMatriz1/3; cA++ ) {
             posA = lA*colunaA + cA;
             posB = lB*colunaB + cB;
             soma = soma + (mat1[posA])*(mat2[posB]);
             lB = lB + 1;
         }
+        printf("%d ", soma);
+        
+        lA = 0;
+        lB = 0; 
+        cB = 2;
+        soma = 0;
 
+        for( cA = 0; cA < tamanhoMatriz1/3; cA++ ) {
+            posA = lA*colunaA + cA;
+            posB = lB*colunaB + cB;
+            soma = soma + (mat1[posA])*(mat2[posB]);
+            lB = lB + 1;
+        }
         printf("%d \n", soma);
-        lA = 1;
+        
+        //Segunda linha
+        lA = 2;
         lB = 0; 
         cB = 0;
         soma = 0;
 
-        for( cA = 0; cA <= tamanhoMatriz1/2; cA++ ) {
+        for( cA = 0; cA <= tamanhoMatriz1/3; cA++ ) {
             posA = lA*colunaA + cA;
             posB = lB*colunaB + cB;
             soma = soma + (mat1[posA])*(mat2[posB]);
             lB = lB + 1;
         }
-
         printf("%d ", soma);
-        lA = 1;
+        
+        lA = 2;
         lB = 0; 
         cB = 1;
         soma = 0;
 
-        for( cA = 0; cA <= tamanhoMatriz1/2; cA++ ) {
+        for( cA = 0; cA <= tamanhoMatriz1/3; cA++ ) {
+            posA = lA*colunaA + cA;
+            posB = lB*colunaB + cB;
+            soma = soma + (mat1[posA])*(mat2[posB]);
+            lB = lB + 1;
+        }
+        printf("%d ", soma);
+        
+        lA = 1;
+        lB = 0; 
+        cB = 2;
+        soma = 0;
+
+        for( cA = 0; cA <= tamanhoMatriz1/4; cA++ ) {
+            posA = lA*colunaA + cA;
+            posB = lB*colunaB + cB;
+            soma = soma + (mat1[posA])*(mat2[posB]);
+            lB = lB + 1;
+        }
+        printf("%d \n", soma);
+        
+        //terceira linha
+        lA = 2;
+        lB = 0; 
+        cB = 0;
+        soma = 0;
+
+        for( cA = 0; cA <= tamanhoMatriz1/3; cA++ ) {
+            posA = lA*colunaA + cA;
+            posB = lB*colunaB + cB;
+            soma = soma + (mat1[posA])*(mat2[posB]);
+            lB = lB + 1;
+        }
+        printf("%d ", soma);
+        
+        lA = 2;
+        lB = 0; 
+        cB = 1;
+        soma = 0;
+
+        for( cA = 0; cA <= tamanhoMatriz1/3; cA++ ) {
+            posA = lA*colunaA + cA;
+            posB = lB*colunaB + cB;
+            soma = soma + (mat1[posA])*(mat2[posB]);
+            lB = lB + 1;
+        }
+        printf("%d ", soma);
+        
+        lA = 2;
+        lB = 0; 
+        cB = 2;
+        soma = 0;
+
+        for( cA = 0; cA <= tamanhoMatriz1/3; cA++ ) {
             posA = lA*colunaA + cA;
             posB = lB*colunaB + cB;
             soma = soma + (mat1[posA])*(mat2[posB]);
